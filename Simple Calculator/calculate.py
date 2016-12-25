2e2068d4fbd7dcf8d3d1502a1d6d07aaf6459623
Harshit Tiwari <harshit.tiwari2015@gmail.com>
2016-12-25 15:52:27 +0530

    Create calculate.py

---
+++ Simple Calculator/calculate.py
@@ -0,0 +1,21 @@
+def add(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
+
+def subtract(smallNumber,largeNumber):
+    difference = 0
+    difference = largeNumber - smallNumber
+    return difference
+
+def multiply(*args):
+    product = 1
+    for i in args:
+        product *= i
+    return product
+
+def divide(dividend, divisor):
+    quotient = dividend/divisor
+    return quotient
+
